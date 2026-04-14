cb8f6f6cad2ee5edd130c208108692b376f7ac5a
Donald Mbajouen <mbajouend@gmail.com>
2026-04-14 16:09:39 +0100

    jour1 avec fastapi

--- votechain-backend/main.py
+++ votechain-backend/main.py
@@ -4,4 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "VoteChain API is running 🚀"}
+    return {"message": "Mon premier hello word depuis fastApi"}
+
+
+@app.get("/hello/{name}")
+def say_hello(name:str):
+    return {"message": f"Hello {name}"}
